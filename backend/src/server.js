9165b23a1476b1feb02b0ae2d0428ecd75cab3ea
Ansh411 <4gansh11@gmail.com>
2025-11-10 03:06:11 +0530

    Created chat endpoints (routes and controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,8 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello There !");
