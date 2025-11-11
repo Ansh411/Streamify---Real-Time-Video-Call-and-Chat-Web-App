73ea80c943acc51b788044e91d629d883cc35002
Ansh411 <4gansh11@gmail.com>
2025-11-12 04:01:41 +0530

    Added real-time chat feature with reactions and personal threads along with image transfer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
         <Route path="/login" element={!isAuthenticated ? <LoginPage/> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />} />
         <Route path="/notifications" element={isAuthenticated && isOnboarded ? (<Layout showSidebar = {true}> <NotificationsPage/>  </Layout> ): (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} /> )} />
         <Route path="/call/:id" element={isAuthenticated && isOnboarded ? (<CallPage/>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />)} />
-        <Route path="/chat:id" element={isAuthenticated && isOnboarded ? (<Layout showSidebar = {true}> <ChatPage/> </Layout>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} /> )} />
+        <Route path="/chat/:id" element={isAuthenticated && isOnboarded ? (<Layout showSidebar = {false}> <ChatPage/> </Layout>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} /> )} />
         <Route path="/onboarding" element={isAuthenticated ? (!isOnboarded ? (<OnboardingPage />) : (<Navigate to="/" />)) : (<Navigate to="/login" />)} />
       </Routes>
 
