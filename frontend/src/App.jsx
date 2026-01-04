939b5297fac76a45075967a5ed83aeb90b317c20
Ansh411 <4gansh11@gmail.com>
2026-01-04 20:00:05 +0530

    Added background to full screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
   }
 
   return (
-    <div className="h-screen" data-theme={theme}>
+    <div className="min-h-screen" data-theme={theme}>
 
       <Routes>
         <Route path="/" element={isAuthenticated && isOnboarded ? (<Layout showSidebar = {true}> <HomePage /> </Layout>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />)} />
